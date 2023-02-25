15a9a0aaec73150e2e748d9825bcf2813f67c24c
liebersonsantos <T3637808@timbrasil.com.br>
2023-02-25 15:48:10 -0300

    lint options

--- app/src/main/java/com/liebxsantos/pexelswallpapersapp/framework/di/NetworkModule.kt
+++ app/src/main/java/com/liebxsantos/pexelswallpapersapp/framework/di/NetworkModule.kt
@@ -1,4 +1,15 @@
 package com.liebxsantos.pexelswallpapersapp.framework.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+    private const val TIMEOUT_SECONDS = 15L
+
+//    @Provides
+//    fun provideLoggingInterceptor(): HttpLoggingInterceptor = 
 }
