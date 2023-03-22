4d51fc7885c545485bc54dfce01c749310cfdc1b
liebersonsantos <T3637808@timbrasil.com.br>
2023-03-22 09:18:33 -0300

    init DB

--- app/src/main/java/com/liebxsantos/pexelswallpapersapp/framework/db/repository/DbRepositoryImpl.kt
+++ app/src/main/java/com/liebxsantos/pexelswallpapersapp/framework/db/repository/DbRepositoryImpl.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class DbRepositoryImpl @Inject constructor(private val dao: WallpapersDao): DbRepository {
-
     override suspend fun insert(photoEntity: PhotoEntity): Long = dao.insert(photoEntity)
     override suspend fun getAllPhotos(): Flow<List<PhotoEntity>> = dao.getAllPhotos()
     override suspend fun deleteById(id: Long) = dao.deleteById(id)
